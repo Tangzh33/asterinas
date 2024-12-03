a6646d2453bbb7f69459cc813057d53b13b4eb31
Zhang Junyang <junyang@stu.pku.edu.cn>
2024-12-03 21:43:45 +0800

    Optimize to flush precisely

--- kernel/src/sched/priority_scheduler.rs
+++ kernel/src/sched/priority_scheduler.rs
@@ -315,7 +315,7 @@ impl PreemptSchedInfo for Thread {
     }
 
     fn cpu_affinity(&self) -> CpuSet {
-        self.atomic_cpu_affinity().load()
+        self.atomic_cpu_affinity().load(Ordering::Relaxed)
     }
 }
 
