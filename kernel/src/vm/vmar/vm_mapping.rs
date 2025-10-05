da20e507e09544143494310eecd42fe9b817d3a9
Zhe Tang <tangzh@stu.pku.edu.cn>
2025-10-05 02:41:27 +0000

    Workaround: Add Write Combining policy for the framebuffer

--- kernel/src/vm/vmar/vm_mapping.rs
+++ kernel/src/vm/vmar/vm_mapping.rs
@@ -194,8 +194,9 @@ impl VmMapping {
         let preempt_guard = disable_preempt();
         let map_range = self.map_to_addr..self.map_to_addr + self.map_size.get();
         let mut cursor = vm_space.cursor_mut(&preempt_guard, &map_range)?;
+        // Use WriteCombining for device memory (e.g., framebuffer) for better performance
         let io_page_prop =
-            PageProperty::new_user(PageFlags::from(self.perms), CachePolicy::Uncacheable);
+            PageProperty::new_user(PageFlags::from(self.perms), CachePolicy::WriteCombining);
         cursor.map_iomem(io_mem, io_page_prop, self.map_size.get(), vmo_offset);
 
         Ok(())
